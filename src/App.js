a5a524fa43d68b927b58c59acee5345b57cbe891
Svturne <mehdi.bad@outook.com>
2023-02-28 17:21:11 +0100

    finished todo-app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import DisplayTodos from "./components/DisplayTodos";
+import Todos from "./components/Todos";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Notes✏️</h1>
+      <Todos />
+      <DisplayTodos />
+    </div>
+  );
+}
+
+export default App;
